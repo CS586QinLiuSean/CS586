9a6673242399f1668b949fbb693418971c9a7668
zishanqin <zqin@wpi.edu>
2015-09-25 22:38:22 -0400

    modify the codes of Task6Reducer

--- HadoopProject/src/Tasks/src/Task6/Task6Reducer.java
+++ HadoopProject/src/Tasks/src/Task6/Task6Reducer.java
@@ -12,34 +12,22 @@ import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
 public class Task6Reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, NullWritable> {
-	public final static int Initial_Time_Unit = 864000;
-    public final static int Current_Time_Unit=10000000;
-	@Override
-	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, NullWritable> output,
-			Reporter report) throws IOException {
-
-//		int count = 0;
-//		while (values.hasNext()) {
-//			IntWritable record = values.next();
-//			if (record.get() == 1) {
-//				count++;
-//			}
-//		}
-//		if (count == 0) {
-//			output.collect(key, NullWritable.get());
-//		}
-		int lastAccess=0;
-		while(values.hasNext()){
-			IntWritable record = values.next();
-			if(record.get()>lastAccess){
-				lastAccess=record.get();
-			}
-		}
-		boolean neverAccess=Current_Time_Unit-lastAccess>Initial_Time_Unit;
-		if(neverAccess){
-			output.collect(key, NullWritable.get());
-		}
-		
-	}
+        
+    @Override
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, NullWritable> output,
+                       Reporter report) throws IOException {
+        
+        int count = 0;
+        while (values.hasNext()) {
+            IntWritable record = values.next();
+            if (record.get() == 1) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            output.collect(key, NullWritable.get());
+        }
+    }
+	
 
 }
